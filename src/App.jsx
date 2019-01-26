1f1eefa999d4b2dcc6ab64c85de03c046b238b50
Tim O'Connell <tim@xch4nge.com>
2019-01-26 15:00:06 +0000

    setup project linting

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
-  render () {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  render() {
     return (
       <div>
         Hello World!
