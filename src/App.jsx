58cc2cead510623965cf1eee09c42146c672246c
Tim O'Connell <tim@xch4nge.com>
2019-01-26 21:28:41 +0000

    cast count to boolean

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ export default class App extends Component {
         >
           Load Candidates
         </button>
-        { rootStore.candidatesCount && <Board store={rootStore} /> }
+        { !!rootStore.candidatesCount && <Board store={rootStore} /> }
       </div>
     )
   }
